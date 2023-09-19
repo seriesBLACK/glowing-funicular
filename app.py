918c798bb1ef2d42733bea8739136497f4441f2a
Abd ul Walee Talal <aboodtalal51@gmail.com>
2023-09-19 14:49:30 +0300

    don't work when tierd

--- app.py
+++ app.py
@@ -62,6 +62,5 @@ def get_progress():
 
 
 if __name__ == "__main__":
-    # from waitress import serve
-    app.run(debug=True)
-    # serve(app, host="0.0.0.0", port=8080)
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
